ef0ce21000f6d4df0608b5cce05a35230797b59c
AyamiYokoi <didufd63hff@gmail.com>
2021-06-10 01:00:51 +0000

    コメント機能搭載中

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_09_115647) do
+ActiveRecord::Schema.define(version: 2021_06_09_233602) do
+
+  create_table "book_comments", force: :cascade do |t|
+    t.text "comment"
+    t.integer "user_id"
+    t.integer "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_book_comments_on_post_id"
+    t.index ["user_id"], name: "index_book_comments_on_user_id"
+  end
 
   create_table "books", force: :cascade do |t|
     t.string "title"
